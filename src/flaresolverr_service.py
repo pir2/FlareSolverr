960c99c23a934875f35cfd3d09542f4ee4485144
Ronson <pir2@users.noreply.github.com>
2024-04-22 16:17:44 -0400

    Update flaresolverr_service.py

--- src/flaresolverr_service.py
+++ src/flaresolverr_service.py
@@ -409,7 +409,7 @@ def _evil_logic(req: V1RequestBase, driver: WebDriver, method: str) -> Challenge
 
         logging.debug("Waiting for 10s")
         time.sleep(10)
-        driver.get(req.url)
+        driver.refresh()
         logging.debug(driver.page_source)
         logging.info("Challenge solved!")
         res.message = "Challenge solved!"
